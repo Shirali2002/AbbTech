83951a7f202319817c331aa4afe55487771d027f
Shirali <shiralialihummatov@gmail.com>
2022-07-30 17:43:43 +0400

    Homework 5 is Done!

--- src/main/java/hw5/Main.java
+++ src/main/java/hw5/Main.java
@@ -4,35 +4,35 @@ import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        Human father = new Human("father", "Adigozelov", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-        Human fatherNew = new Human("father", "Adigozelov", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-
-
-
-        Human mother = new Human("mother", "Adigozelova", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-        Human motherNew = new Human("mother", "Adigozelova", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-
-        Human child1 = new Human("child1", "Adigozelova", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-        Human child1New = new Human("child1", "Adigozelova", 2012, 50,
-                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//        Human father = new Human("father", "Adigozelov", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//        Human fatherNew = new Human("father", "Adigozelov", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//
+//
+//
+//        Human mother = new Human("mother", "Adigozelova", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//        Human motherNew = new Human("mother", "Adigozelova", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//
+//        Human child1 = new Human("child1", "Adigozelova", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
+//        Human child1New = new Human("child1", "Adigozelova", 2012, 50,
+//                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
 
 //        Human child2 = new Human("child2", "Adigozelova", 2012, 50,
 //                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
 //        Human child3 = new Human("child3", "Adigozelova", 2012, 50,
 //                new String[][]{{"monday", "do homework"}, {"saturday", "Take the dog for a walk."}});
-
-        Pet toplan = new Pet("dog", "Toplan", 3, 65, "eat", "drink", "sleep", "run");
-        Pet toplanNew = new Pet("dog", "Toplan", 3, 65, "eat", "drink", "sleep", "run");
+//
+//        Pet toplan = new Pet("dog", "Toplan", 3, 65, "eat", "drink", "sleep", "run");
+//        Pet toplanNew = new Pet("dog", "Toplan", 3, 65, "eat", "drink", "sleep", "run");
 
 //        Pet mestan = new Pet("dog", "Toplan", 3, 65, new String[]{"eat", "drink", "sleep", "run"});
 
-        Family family1 = new Family(mother, father, toplan, child1);
-        Family family2 = new Family(motherNew, fatherNew, toplanNew, child1New);
+//        Family family1 = new Family(mother, father, toplan, child1);
+//        Family family2 = new Family(motherNew, fatherNew, toplanNew, child1New);
 
 //        System.out.println(family1.equals(family2));
 
