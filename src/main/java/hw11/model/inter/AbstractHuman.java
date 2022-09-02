8337acf909c9e930291b6c91d949bf214bc68313
Shirali <shiralialihummatov@gmail.com>
2022-09-02 10:16:02 +0400

    Homework 11 is Done!

--- src/main/java/hw11/model/inter/AbstractHuman.java
+++ src/main/java/hw11/model/inter/AbstractHuman.java
@@ -160,6 +160,7 @@ public abstract class AbstractHuman {
     return String.format("%d/%d/%d", calendar.get(Calendar.DAY_OF_MONTH),
         calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.YEAR));
   }
+
   public boolean feedPet(boolean isTimeFeed) {
     if (hasNotPet()) {
       return false;
